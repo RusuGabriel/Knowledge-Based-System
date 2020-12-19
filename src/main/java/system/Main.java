7fafb68bd31826164605fbbf207944dccc83b9ad
Gabriel Rusu <rusugabriel13@gmail.com>
2020-12-19 21:52:22 +0200

    now the path to resources is added

--- src/main/java/system/Main.java
+++ src/main/java/system/Main.java
@@ -1,5 +1,7 @@
 package system;
 
+import java.io.File;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -10,12 +12,14 @@ import javafx.stage.Stage;
  * Hello world!
  *
  */
-public class App extends Application {
+public class Main extends Application {
+    private static final File resources = new File("src/main/resources");
 
     @Override
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
+        System.out.println(resources.listFiles());
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         Scene scene = new Scene(new StackPane(l), 640, 480);
         
