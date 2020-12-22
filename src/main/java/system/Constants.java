e9cfc93c905c6c58d9ab80e16548139f1c04a952
Gabriel Rusu <rusugabriel13@gmail.com>
2020-12-22 20:34:38 +0200

    updated the project

--- src/main/java/system/Constants.java
+++ src/main/java/system/Constants.java
@@ -6,11 +6,11 @@ package system;
 public final class Constants {
     public static final class Screens {
         public static final String HOME = "/templates/HomeScreen.fxml";
+        public static final String KNOWLEDGE_BASE = "/templates/KnowledgeBaseScreen.fxml";
 
         private Screens() {
         }
     }
 
-    private Constants(){};
-
+    private Constants(){}
 }
