dc0633721d0cdd056a3bc6e1c18f77a01e3281d0
Gabriel Rusu <rusugabriel13@gmail.com>
2020-12-23 15:05:17 +0200

    now the KnowledgeStore is a singleton thread safe

--- src/main/java/system/models/KnowledgeStore.java
+++ src/main/java/system/models/KnowledgeStore.java
@@ -1,5 +1,19 @@
 package system.models;
 
 public class KnowledgeStore {
-    
+    private static KnowledgeStore instance = null;
+
+    private KnowledgeStore() {
+    }
+
+    public static KnowledgeStore getInstance() {
+        if (instance == null) {
+            synchronized (KnowledgeStore.class) {
+                if (instance == null)
+                    instance = new KnowledgeStore();
+            }
+        }
+        return instance;
+    }
+
 }
