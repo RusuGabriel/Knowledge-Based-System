a8eb70c0ea1cd4d385bbfc7baf70a3a1a6e452bd
Gabriel Rusu <rusugabriel13@gmail.com>
2020-12-23 15:20:52 +0200

    added the knoledge.xml + implementation in progress for loadingKnowledge

--- src/main/java/system/models/KnowledgeStore.java
+++ src/main/java/system/models/KnowledgeStore.java
@@ -1,7 +1,14 @@
 package system.models;
 
+import java.util.ArrayList;
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
+import org.xml.sax.*;
+
 public class KnowledgeStore {
     private static KnowledgeStore instance = null;
+    private ArrayList<Phone> phoneStore = null;
 
     private KnowledgeStore() {
     }
@@ -14,6 +21,27 @@ public class KnowledgeStore {
             }
         }
         return instance;
+    }
+
+    public void loadKnowledge() throws ParserConfigurationException, SAXException, IOException {
+        phoneStore = new ArrayList<>();
+        // Get Document Builder
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        // Build Document
+        Document document = builder.parse(new File("employees.xml"));
+
+        // Normalize the XML Structure; It's just too important !!
+        document.getDocumentElement().normalize();
+
+        // Here comes the root nodeF
+        Element root = document.getDocumentElement();
+        System.out.println(root.getNodeName());
+
+        // Get all employees
+        NodeList nList = document.getElementsByTagName("employee");
+        System.out.println("============================");
     }
 
 }
