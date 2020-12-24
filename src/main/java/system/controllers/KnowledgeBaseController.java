5585f1570d24a4a3eaf2d4706c013ee11db05465
Gabriel Rusu <rusugabriel13@gmail.com>
2020-12-24 21:25:06 +0200

    added new knoledge info

--- src/main/java/system/controllers/KnowledgeBaseController.java
+++ src/main/java/system/controllers/KnowledgeBaseController.java
@@ -33,26 +33,26 @@ public class KnowledgeBaseController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         knowledgeRepository.loadKnowledge();
         brandChoice.setItems(FXCollections.observableArrayList(knowledgeRepository.getAllBrands()));
-        
+
         TableColumn<ElectronicDevice, String> modelCol = new TableColumn<>("Model");
         modelCol.setCellValueFactory(new PropertyValueFactory<ElectronicDevice, String>("Model"));
         modelCol.setCellFactory(TextFieldTableCell.forTableColumn());
         modelCol.prefWidthProperty().bind(knowledgeTable.widthProperty().multiply(0.33));
         modelCol.setEditable(false);
-        
+
         TableColumn<ElectronicDevice, String> brandCol = new TableColumn<>("Brand");
         brandCol.setCellValueFactory(new PropertyValueFactory<ElectronicDevice, String>("Brand"));
         brandCol.setCellFactory(TextFieldTableCell.forTableColumn());
         brandCol.prefWidthProperty().bind(knowledgeTable.widthProperty().multiply(0.33));
         brandCol.setEditable(false);
-        
+
         TableColumn<ElectronicDevice, Integer> yearCol = new TableColumn<>("Year");
         yearCol.setCellValueFactory(new PropertyValueFactory<ElectronicDevice, Integer>("Year"));
         yearCol.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
         yearCol.prefWidthProperty().bind(knowledgeTable.widthProperty().multiply(0.33));
         yearCol.setEditable(false);
 
-        knowledgeTable.getColumns().addAll(modelCol,brandCol, yearCol);
+        knowledgeTable.getColumns().addAll(modelCol, brandCol, yearCol);
         knowledgeTable.getItems().addAll(FXCollections.observableArrayList(knowledgeRepository.getAll()));
         knowledgeTable.setEditable(false);
     }
