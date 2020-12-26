dbfca9ef7c310eded8e30117eea113db00b11ac3
Gabriel Rusu <rusugabriel13@gmail.com>
2020-12-26 19:10:18 +0200

    added the missing method

--- src/main/java/system/controllers/RulesController.java
+++ src/main/java/system/controllers/RulesController.java
@@ -3,7 +3,10 @@ package system.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import system.App;
+import system.utils.Constants;
 
 public class RulesController implements Initializable {
 
@@ -11,6 +14,11 @@ public class RulesController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         // TODO Auto-generated method stub
 
+    }
+
+    @FXML
+    public void goBack() {
+        App.display(Constants.Screens.HOME);
     }
     
 }
